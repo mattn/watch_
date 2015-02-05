4deed55ab9778602e21d01b7e74bd2d6a21179f9
omeid <public@omeid.me>
2015-02-05 09:36:49 -0500

    init

---
+++ watch.go
@@ -0,0 +1,46 @@
+package watch
+
+import (
+	"io"
+	"log"
+
+	"github.com/omeid/slurp/s"
+	"github.com/omeid/slurp/tools/glob"
+	"gopkg.in/fsnotify.v1"
+)
+
+func Watch(c *s.C, task func(), globs ...string) io.Closer {
+
+	files, err := glob.Glob(globs...)
+
+	if err != nil {
+		c.Println(err)
+		return nil
+	}
+
+	w, err := fsnotify.NewWatcher()
+	if err != nil {
+		c.Println(err)
+		return nil
+	}
+
+	for matchpair := range files {
+		w.Add(matchpair.Name)
+	}
+
+	go func() {
+		for {
+			select {
+			case event := <-w.Events:
+				if event.Op&fsnotify.Write == fsnotify.Write {
+					log.Println("modified file:", event.Name)
+					task()
+				}
+			case err := <-w.Errors:
+				c.Println(err)
+			}
+		}
+	}()
+
+	return w
+}
